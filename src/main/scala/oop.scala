54dbe5191934b35e1bc2c101458cbfbf2494ac61
qubits <ameet@siglabs.com>
2019-04-26 23:37:04 -0700

    finish oop

--- src/main/scala/oop.scala
+++ src/main/scala/oop.scala
@@ -9,12 +9,20 @@ class Student1(id:Int, name:String) {
   }
 }
 
+class Arithmetic {
+  def add(a:Int,b:Int) {
+    var add = a+b
+    println("sum ="+add)
+  }
+}
+
 object OOP{
   def main(args:Array[String]) {
     var s0 = new Student0()
     println(s0.id+" "+s0.name)
     var s1 = new Student1(100,"Ameet")
     s1.show()
+    new Arithmetic().add(10,10)
   }
 }
 

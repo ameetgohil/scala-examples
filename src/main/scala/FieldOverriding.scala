a4f67be6a7986949f192dc1ba93441eeed671de1
qubits <ameet@siglabs.com>
2019-04-27 15:45:40 -0700

    add field overriding

---
+++ src/main/scala/FieldOverriding.scala
@@ -0,0 +1,17 @@
+// can only override immutable
+class Vehicle{
+  val speed:Int = 60
+  
+}
+class Bike extends Vehicle{
+  override val speed:Int = 100     // Override keyword
+  def show(){
+    println(speed)
+  }
+}
+object FieldOverriding {
+  def main(args:Array[String]){
+    var b = new Bike()
+    b.show()
+  }
+}
